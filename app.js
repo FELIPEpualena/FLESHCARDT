3727786cde2a9c54e9d9bf29365ad94ae9076be3
FELIPEpualena <felipe.paulena@escola.pr.gov.br>
2025-08-06 13:55:27 +0000

    gg

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ function criaCartão(categoria, pergunta, resposta){
 const container = document.getElementById('container')
 const cartao = document.creaElement('article')
 cartao.className = 'carrtao'
-    
-    
-    
 
+cartao.innerHTML = `<div class="cartao_conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="pergunta">
+                         ${pergunta}
+                    </div>
+                    <div class="resposta">
+                         ${resposta}
+                    </div>
+                </div>´
+
+                    container.appendChild (cartao)
 
 }
